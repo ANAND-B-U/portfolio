1af8d4702d0b6e533a0027ea65fa351e1ade5429
ANAND-B-U <b.aanand2233@gmail.com>
2026-09-19 19:55:27 +0530

    Modified some designs in footer and scrollbar

--- src/components/common/footer/Footer.jsx
+++ src/components/common/footer/Footer.jsx
@@ -6,7 +6,7 @@ const copyrightYear = new Date().getFullYear();
 
 const Footer = () => {
   return (
-    <footer className="bg-[#050608] border-t border-white/10 text-neutral-200 py-6 px-6">
+    <footer className="bg-[#050608] border-t border-white/10 text-neutral-200 py-2 px-6">
       <div className="flex flex-col gap-4 sm:flex-row items-center justify-between max-w-6xl mx-auto">
         <div className="flex items-center gap-4">
           <div className="w-12 h-12 rounded-full bg-white/10 border border-white/10 flex items-center justify-center text-lg font-bold text-white">
